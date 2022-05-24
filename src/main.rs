2963ab82069ae2a5c0c5d4227900a6c153b9b41b
Leonardo F <leonardo@essia.dev>
2022-05-24 12:36:51 -0300

    fix the merge i've fucked up

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,3 @@
-<<<<<<< HEAD
-mod app;
-mod mongo;
-mod ui;
-use crate::app::{run_app, App};
-
-use crossterm::{
-    event::{DisableMouseCapture, EnableMouseCapture},
-    execute,
-    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
-};
-use std::io;
-use std::time::Duration;
-use tui::{backend::CrosstermBackend, Terminal};
-
-fn main() -> Result<(), io::Error> {
-    // setup terminal
-    enable_raw_mode()?;
-    let mut stdout = io::stdout();
-    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
-    let backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
-
-    // create app and run it
-    let tick_rate = Duration::from_millis(250);
-    let app = App::new();
-    let res = run_app(&mut terminal, app, tick_rate);
-
-    // restore terminal
-    disable_raw_mode()?;
-    execute!(
-        terminal.backend_mut(),
-        LeaveAlternateScreen,
-        DisableMouseCapture
-    )?;
-    terminal.show_cursor()?;
-
-    if let Err(err) = res {
-        println!("{:?}", err)
-    }
-
-    Ok(())
-}
-||||||| (empty tree)
-=======
 use crossterm::{
     event::{DisableMouseCapture, EnableMouseCapture},
     execute,
@@ -164,4 +119,3 @@ fn draw_preview<B: Backend>(f: &mut Frame<B>, area: Rect) {
         .style(Style::default().bg(Color::Black));
     f.render_widget(block, chunks[0]);
 }
->>>>>>> 7f59395 (create the layout only)
