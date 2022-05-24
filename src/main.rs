ea6393823b20118d104df7982989b5bfe8608470
Leonardo F <leonardo@essia.dev>
2022-05-24 12:03:13 -0300

    create the layout only

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 mod app;
 mod mongo;
 mod ui;
@@ -40,3 +41,127 @@ fn main() -> Result<(), io::Error> {
 
     Ok(())
 }
+||||||| (empty tree)
+=======
+use crossterm::{
+    event::{DisableMouseCapture, EnableMouseCapture},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use std::{io, thread, time::Duration};
+use tui::{
+    backend::{Backend, CrosstermBackend},
+    layout::{Alignment, Constraint, Direction, Layout, Rect},
+    style::{Color, Modifier, Style},
+    text::{Span, Spans},
+    widgets::{Block, BorderType, Borders, List, ListItem, Paragraph, Wrap},
+    Frame, Terminal,
+};
+
+fn main() -> Result<(), io::Error> {
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    terminal.draw(ui)?;
+
+    thread::sleep(Duration::from_millis(5000));
+
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    Ok(())
+}
+
+fn ui<B: Backend>(f: &mut Frame<B>) {
+    let chunks = Layout::default()
+        .margin(1)
+        .constraints([Constraint::Length(3), Constraint::Min(0)].as_ref())
+        .split(f.size());
+
+    draw_input(f, chunks[0]);
+
+    {
+        let chunks = Layout::default()
+            .constraints([Constraint::Percentage(30), Constraint::Percentage(80)].as_ref())
+            .direction(Direction::Horizontal)
+            .split(chunks[1]);
+
+        draw_database_tree(f, chunks[0]);
+        draw_preview(f, chunks[1]);
+    }
+}
+
+fn draw_input<B: Backend>(f: &mut Frame<B>, area: Rect) {
+    let text = vec![Spans::from(Span::styled(
+        "type here...",
+        Style::default().fg(Color::DarkGray),
+    ))];
+
+    let block = Block::default()
+        .borders(Borders::ALL)
+        .border_type(BorderType::Rounded)
+        .title(Span::styled(
+            "Input",
+            Style::default()
+                .add_modifier(Modifier::BOLD)
+                .fg(Color::Gray),
+        ));
+    let paragraph = Paragraph::new(text).block(block).wrap(Wrap { trim: true });
+    f.render_widget(paragraph, area);
+}
+
+fn draw_database_tree<B: Backend>(f: &mut Frame<B>, area: Rect) {
+    let chunks = Layout::default()
+        .constraints([Constraint::Percentage(100)])
+        .direction(Direction::Vertical)
+        .split(area);
+
+    {
+        let chunks = Layout::default()
+            .constraints([Constraint::Percentage(100)])
+            .direction(Direction::Vertical)
+            .split(chunks[0]);
+
+        let items = [
+            ListItem::new("> admin"),
+            ListItem::new("> config"),
+            ListItem::new("> local"),
+        ];
+        let databases = List::new(items)
+            .block(
+                Block::default()
+                    .title("Databases")
+                    .title_alignment(Alignment::Center)
+                    .borders(Borders::ALL)
+                    .border_type(BorderType::Rounded),
+            )
+            .style(Style::default().fg(Color::White))
+            .highlight_style(Style::default().add_modifier(Modifier::ITALIC))
+            .highlight_symbol("> ");
+        f.render_widget(databases, chunks[0]);
+    }
+}
+
+fn draw_preview<B: Backend>(f: &mut Frame<B>, area: Rect) {
+    let chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([Constraint::Percentage(100)])
+        .split(area);
+
+    let block = Block::default()
+        .title("Preview")
+        .title_alignment(Alignment::Center)
+        .borders(Borders::ALL)
+        .border_style(Style::default().fg(Color::White))
+        .border_type(BorderType::Rounded)
+        .style(Style::default().bg(Color::Black));
+    f.render_widget(block, chunks[0]);
+}
+>>>>>>> 7f59395 (create the layout only)
