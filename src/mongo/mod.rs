617907e9fead8b4f92ac9ada9ed97f044ce361dd
Leonardo Essia <leonardo@essia.dev>
2022-07-31 15:24:25 -0300

    feat: add functions to fetch all objects in mongo
    relates to #6

--- src/mongo/mod.rs
+++ src/mongo/mod.rs
@@ -1,9 +1,11 @@
+use mongodb::bson::doc;
 use mongodb::error::Result;
+
 use mongodb::sync::Client;
 use std::env;
 
 // pub async fn get_dbs() -> Result<Vec<String>, Box<dyn Error>> {
-pub fn get_dbs() -> Result<Vec<String>> {
+pub fn get_database_names() -> Result<Vec<String>> {
     // Load the MongoDB connection string from an environment variable:
     let client_uri =
         env::var("MONGODB_URI").expect("You must set the MONGODB_URI environment var!");
@@ -12,4 +14,50 @@ pub fn get_dbs() -> Result<Vec<String>> {
     let client = Client::with_uri_str(client_uri)?;
 
     client.list_database_names(None, None)
+}
+
+pub fn get_collections_from_db(database: String) -> Result<Vec<String>> {
+    let client_uri =
+        env::var("MONGODB_URI").expect("You must set the MONGODB_URI environment var!");
+
+    let client = Client::with_uri_str(client_uri)?;
+
+    client
+        .database(&database)
+        .list_collection_names(doc! {"type": "collection"})
+}
+
+pub fn get_views_from_db(database: String) -> Result<Vec<String>> {
+    let client_uri =
+        env::var("MONGODB_URI").expect("You must set the MONGODB_URI environment var!");
+
+    let client = Client::with_uri_str(client_uri)?;
+
+    client
+        .database(&database)
+        .list_collection_names(doc! {"type": "view"})
+}
+
+pub fn get_users_from_db(database: String) -> Result<Vec<String>> {
+    let client_uri =
+        env::var("MONGODB_URI").expect("You must set the MONGODB_URI environment var!");
+
+    let client = Client::with_uri_str(client_uri)?;
+
+    let all_users = client
+        .database(&database)
+        .run_command(doc! {"usersInfo": 1}, None)
+        .unwrap();
+
+    let mut users: Vec<String> = Vec::new();
+
+    all_users
+        .get_array("users")
+        .unwrap()
+        .iter()
+        .for_each(|user| {
+            users.push(user.as_document().unwrap().get("user").unwrap().to_string());
+        });
+
+    Ok(users)
 }
