99d8b07a0586ceae16068b04ed72cdae4389efc4
Leonardo Essia <leonardo@essia.dev>
2022-07-31 18:54:12 -0300

    feat: "improve" colors when focusing blocks

--- src/ui.rs
+++ src/ui.rs
@@ -32,8 +32,11 @@ fn draw_input<B: Backend>(f: &mut Frame<B>, app: &App, area: Rect) {
     let block = Block::default()
         .borders(Borders::ALL)
         .border_type(BorderType::Rounded)
+        .border_style(match app.focus {
+            Some(Focus::InputBlock) => Style::default().fg(Color::Cyan),
+            _ => Style::default().fg(Color::White),
+        })
         .title(Span::raw("Input"));
-    // .title(Span::styled("Input", Style::default().fg(Color::Gray)));
 
     let (input_normal_mode_message, input_normal_mode_style) = (
         vec![
@@ -50,13 +53,7 @@ fn draw_input<B: Backend>(f: &mut Frame<B>, app: &App, area: Rect) {
     text.patch_style(input_normal_mode_style);
 
     let input = match app.input_mode {
-        InputMode::Insert => Paragraph::new(app.input.as_ref())
-            .style(
-                Style::default()
-                    .add_modifier(Modifier::BOLD)
-                    .fg(Color::Cyan),
-            )
-            .block(block),
+        InputMode::Insert => Paragraph::new(app.input.as_ref()).block(block),
         InputMode::Normal => Paragraph::new(text).style(Style::default()).block(block),
     };
     // TODO: better insert text formatting (add margin to input field,
@@ -127,14 +124,14 @@ fn draw_database_tree<B: Backend>(f: &mut Frame<B>, app: &mut App, area: Rect) {
                     .title("Databases")
                     .title_alignment(Alignment::Center)
                     .borders(Borders::ALL)
-                    .border_type(BorderType::Rounded),
+                    .border_type(BorderType::Rounded)
+                    .border_style(match app.focus {
+                        Some(Focus::DatabaseBlock) => Style::default().fg(Color::Cyan),
+                        _ => Style::default().fg(Color::White),
+                    }),
             )
-            .style(match app.focus {
-                Some(Focus::DatabaseBlock) => Style::default().fg(Color::Cyan),
-                _ => Style::default().fg(Color::White),
-            })
             .highlight_style(Style::default().add_modifier(Modifier::ITALIC))
-            .highlight_symbol(" ");
+            .highlight_symbol("");
 
         let mut state = ListState::default();
         state.select(app.database_selected);
