0ad695ed504971f51a81482c0b8251e209e78b50
Leonardo Essia <leonardo@essia.dev>
2022-12-22 10:31:42 -0300

    feat: improve code organization
    Signed-off-by: Leonardo Essia <leonardo@essia.dev>

--- bongo/app.go
+++ bongo/app.go
@@ -3,13 +3,17 @@ package bongo
 import "github.com/rivo/tview"
 
 type App struct {
-	app   *tview.Application
-	pages *tview.Pages
+	app      *tview.Application
+	pages    *tview.Pages
+	treeNode *tview.TreeNode
+	treeView *tview.TreeView
 }
 
 func Init() *App {
 	return &App{
-		app:   tview.NewApplication(),
-		pages: tview.NewPages(),
+		app:      tview.NewApplication(),
+		pages:    tview.NewPages(),
+		treeNode: tview.NewTreeNode("."),
+		treeView: tview.NewTreeView(),
 	}
 }
